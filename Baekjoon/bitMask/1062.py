9b90d0e835df5997dc99ea57ea00ded05bb4ded9
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-01-15 17:49:37 +0900

    Fix solving 1062 - 21/01/15

--- Baekjoon/bitMask/1062.py
+++ Baekjoon/bitMask/1062.py
@@ -1,10 +1,12 @@
 from collections import deque
+from itertools import combinations
 N, K = map(int, input().split())
 alphabets = ['a', 'n', 't', 'i', 'c']
+K -= 5
 new_list = []
 answer = 0
-for i in range(K):
-    if K < 5:
+for i in range(N):
+    if K < 1:
         break
     dq = deque()
     input_ = input()
@@ -20,17 +22,19 @@ for i in range(K):
             dq.remove(letter)
     str = ''
     for a in dq:
-        if a not in new_list:
-            str += a
+        str += a
     new_list.append(str)
 
-remainder = ['b','d','e','f','g','h','j','k','l','m','o','p','q','r','s','u','v','w','x','y','z']
-nums = [0] * 21
-idx = 0
-while True:
-    if len(new_list) == 0:
-        break
-    # if new_list[idx] == ' ':
+new_list.sort(key=len)
+
+remainder = {'b':0,'d':0,'e':0,'f':0,'g':0,'h':0,
+             'j':0,'k':0,'l':0,'m':0,'o':0,'p':0,
+             'q':0,'r':0,'s':0,'u':0,'v':0,'w':0,
+             'x':0,'y':0,'z':0}
+letters = []
+for i in list(combinations(remainder, K)):
+    for j in i:
+        if j in new_list:
 
 
 
