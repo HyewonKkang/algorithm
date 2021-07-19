793c225d41a76c7485eab47f842319b410bb1854
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-07-19 17:22:06 +0900

    Add divisors

---
+++ Programmers/level1/divisors.py
@@ -0,0 +1,17 @@
+def getNumOfdivisors(num):
+    cnt = 0
+    for i in range(1, num+1):
+        if num % i == 0:
+            cnt += 1
+    return cnt
+
+def solution(left, right):
+    answer = 0
+
+    for num in range(left, right+1):
+        if getNumOfdivisors(num) % 2 == 0:
+            answer += num
+        else:
+            answer -= num
+
+    return answer
