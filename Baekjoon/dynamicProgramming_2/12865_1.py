431a36663444d7205508bebffc04dbc8b06d9ee9
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2022-05-19 11:02:15 +0900

    Add solved 12865

---
+++ Baekjoon/dynamicProgramming_2/12865_1.py
@@ -0,0 +1,13 @@
+stuff = []
+dp = [0] * (k + 1)
+
+for _ in range(n):
+    w, v = map(int, input().split())
+    stuff.append((w, v))
+stuff.sort()
+
+for s in stuff:
+    for i in range(k, s[0] - 1, -1):
+        dp[i] = max(dp[i], dp[i - s[0]] + s[1])
+
+print(dp[-1])
