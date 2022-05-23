0b79e648c616d2884670f564e44adf1e1c543b42
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2022-05-23 14:07:08 +0900

    Add solved 2293

---
+++ Baekjoon/dynamicProgramming_1/2293.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+values = [int(input()) for _ in range(n)]
+dp = [0] * (k + 1)
+dp[0] = 1
+for v in values:
+    for i in range(v, k + 1):
+        dp[i] += dp[i - v]
+
+print(dp[k])
