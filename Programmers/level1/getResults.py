23830238ef295b48a9a2cb421f3f6fd01c744968
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2023-03-27 18:11:18 +0900

    Add solved getResults

--- Programmers/level1/getResults.py
+++ Programmers/level1/getResults.py
@@ -1,24 +1,36 @@
+# Apr 21, 2022
+# def solution(id_list, report, k):
+#     answer = []
+#     reports = {}
+#     counts = {}
+#     for id in id_list:
+#         reports[id] = []
+#         counts[id] = 0
+#     for content in report:
+#         a, b = content.split()
+#         if b not in reports[a]:
+#             reports[a].append(b)
+#             counts[b] += 1
+#
+#     for id in id_list:
+#         cnt = 0
+#         for user in reports[id]:
+#             if counts[user] >= k:
+#                 cnt += 1
+#         answer.append(cnt)
+#     return answer
+
 def solution(id_list, report, k):
-    answer = []
-    reports = {}
-    counts = {}
-    for id in id_list:
-        reports[id] = []
-        counts[id] = 0
-    for content in report:
-        a, b = content.split()
-        if b not in reports[a]:
-            reports[a].append(b)
-            counts[b] += 1
+    answer = [0 for _ in id_list]
+    counts = {user: 0 for user in id_list} # 신고된 횟수
+    dic = {user: [] for user in id_list} # 유저:신고한 사람 dict
+    for r in set(report):
+        user, reported = r.split()
+        counts[reported] += 1
+        dic[user].append(reported)
 
-    for id in id_list:
-        cnt = 0
-        for user in reports[id]:
-            if counts[user] >= k:
-                cnt += 1
-        answer.append(cnt)
+    for user, reported in dic.items():
+        for rId in reported:
+            if counts[rId] >= k:
+                answer[id_list.index(user)] += 1
     return answer
-
-
-print(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
-print(solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
