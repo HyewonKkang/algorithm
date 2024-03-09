4589f356b9630806685468f8d1bdf08337894d20
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2024-03-09 13:55:36 +0900

    Add solved 2110

---
+++ Baekjoon/binarySearch/2110.py
@@ -0,0 +1,25 @@
+n, c = map(int, input().split())
+houses = [int(input()) for _ in range(n)]
+houses.sort()
+answer = 0
+
+start, end = 1, houses[-1] - houses[0]
+
+while start <= end:
+    mid = (start + end) // 2
+    current = houses[0]
+    count = 1
+
+    for i in range(1, n):
+        if houses[i] - current >= mid:
+            count += 1
+            current = houses[i]
+
+    if count >= c:
+        start = mid + 1
+        answer = mid
+    else:
+        end = mid - 1
+
+
+print(answer)
